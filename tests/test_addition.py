fa373065db1c27354abbe479d85b75632c87d232
antai <ai337@njit.edu>
2024-01-28 16:58:13 -0500

    fixed comma

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -4,4 +4,4 @@ from app.addition import addition
 
 def test_addition():
     """Adds numbers"""
-    assert addition(2, 2) == 4 "Addition is not working."
+    assert addition(2, 2) == 4, "Addition is not working."
